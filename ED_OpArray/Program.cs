57de6d127494d3287337a811f2a30371118de260
Antonio Rogel <arogel@unikino.edu.mx>
2020-07-18 12:37:20 -0700

    Agregando opciones de ValidaLleno, ValidaExiste, Agregar

--- ED_OpArray/Program.cs
+++ ED_OpArray/Program.cs
@@ -9,18 +9,14 @@ namespace ED_OpArray
         {
             try
             {
-                ValidaVacio();
-
-                Arreglo[0] = "Cero";
-
-                ValidaVacio();
+                Agregar("Cero");
+                Agregar("Cero");
             }
             catch (Exception ex)
             {
                 Console.WriteLine(ex.Message);
             }
         }
-
         private static bool ValidaVacio() 
         {
             if (Arreglo[0] == null) 
@@ -30,6 +26,63 @@ namespace ED_OpArray
             else 
             {
                 return false;
+            }
+        }
+        private static bool ValidaLleno() 
+        {
+            if (Arreglo[Arreglo.Length -1] != null) 
+            {
+                return true;
+            }
+            else 
+            {
+                return false;
+            }
+        }
+        private static bool ValidaExiste(string Dato)
+        {
+            if (!ValidaVacio()) //Si el arreglo no está vacío
+            {
+                foreach(string registro in Arreglo) 
+                {
+                    if (registro == null) 
+                    {
+                        return false;
+                    }
+
+                    if (registro.ToUpper() == Dato.ToUpper()) 
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+        private static void Agregar(string Dato) 
+        {
+            try
+            {
+                if (ValidaLleno()) 
+                {
+                    throw new Exception("El arreglo está lleno!");
+                }
+
+                if (!ValidaExiste(Dato)) 
+                {
+                    int arregloLength = Arreglo.Length;
+                    for (int i = 0; i < arregloLength; i++) 
+                    {
+                        if (Arreglo[i] == null) 
+                        {
+                            Arreglo[i] = Dato;
+                            break;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
             }
         }
     }
